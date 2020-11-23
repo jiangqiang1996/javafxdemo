1bd8d9eafa37e3dec1dfb7049698dbc836abc97a
jiangqiang <jq1257403419@vip.qq.com>
2020-11-23 14:42:17 +0800

    注释模块化文件

--- javafxother/src/main/java/module-info.java
+++ javafxother/src/main/java/module-info.java
@@ -2,6 +2,6 @@ module xin.jiangqiang {
     requires javafx.controls;
     requires javafx.fxml;
 
-    opens xin.jiangqiang to javafx.fxml;
-    exports xin.jiangqiang;
+//    opens xin.jiangqiang to javafx.fxml;
+//    exports xin.jiangqiang;
 }
